a4e287bf9074a7b618ce03e80aba97dcab414ad5
izzy2lost <izzynochill@gmail.com>
2025-06-09 13:28:03 -0400

    icon change

--- Android/app/src/main/cpp/native-lib.cpp
+++ Android/app/src/main/cpp/native-lib.cpp
@@ -1,1 +1,20 @@
-// native-lib.cpp placeholder
+#include <jni.h>
+#include <string>
+#include <android/log.h>
+
+extern "C" int main(int argc, char* argv[]);
+
+extern "C" JNIEXPORT void JNICALL
+Java_com_starship_MainActivity_nativeInit(JNIEnv* env, jobject /* this */, jstring romPath) {
+    const char* path = env->GetStringUTFChars(romPath, nullptr);
+    __android_log_print(ANDROID_LOG_INFO, "Starship", "ROM Path: %s", path);
+
+    char* argv[] = {
+        const_cast<char*>("starship"),
+        const_cast<char*>(path),
+    };
+
+    main(2, argv);
+
+    env->ReleaseStringUTFChars(romPath, path);
+}
--- Android/app/src/main/java/com/starship/MainActivity.java
+++ Android/app/src/main/java/com/starship/MainActivity.java
@@ -1,1 +1,63 @@
-// MainActivity.java placeholder
+package com.starship;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.DocumentsContract;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+
+public class MainActivity extends Activity {
+    static {
+        System.loadLibrary("native-lib");
+    }
+
+    private static final int PICK_ROM_REQUEST = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        pickRom();
+    }
+
+    private void pickRom() {
+        Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+        intent.setType("*/*");
+        intent.addCategory(Intent.CATEGORY_OPENABLE);
+        startActivityForResult(intent, PICK_ROM_REQUEST);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == PICK_ROM_REQUEST && resultCode == RESULT_OK && data != null) {
+            Uri uri = data.getData();
+            try {
+                InputStream inputStream = getContentResolver().openInputStream(uri);
+                File romFile = new File(getFilesDir(), "baserom.z64");
+                FileOutputStream outputStream = new FileOutputStream(romFile);
+
+                byte[] buffer = new byte[4096];
+                int length;
+                while ((length = inputStream.read(buffer)) > 0) {
+                    outputStream.write(buffer, 0, length);
+                }
+
+                inputStream.close();
+                outputStream.close();
+
+                nativeInit(romFile.getAbsolutePath());
+
+            } catch (Exception e) {
+                e.printStackTrace();
+                Toast.makeText(this, "Failed to load ROM", Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+    public native void nativeInit(String romPath);
+}
