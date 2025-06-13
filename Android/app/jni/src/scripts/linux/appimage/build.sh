a455d1be0dded5e63b469ae14335791d259c242f
izzy2lost <izzynochill@gmail.com>
2025-06-13 15:18:09 -0400

    Delete non android scripts

--- Android/app/jni/src/scripts/linux/appimage/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cmake --no-warn-unused-cli -H. -Bbuild-cmake -GNinja -DCMAKE_BUILD_TYPE:STRING=Release
-cmake --build build-cmake --config Release --
-
-(cd build-cmake && cpack -G External)
--- Android/app/jni/src/scripts/wiiu/build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-cmake -H. -Bbuild-wiiu -GNinja -DCMAKE_TOOLCHAIN_FILE=/opt/devkitpro/cmake/WiiU.cmake -DCMAKE_BUILD_TYPE:STRING=Release
-cmake --build build-wiiu --target soh_wuhb --config Release
