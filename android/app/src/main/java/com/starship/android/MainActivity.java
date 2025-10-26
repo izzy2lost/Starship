13b532737a34867e0e366e2e27cb7de2024ab687
izzy2lost <izzynochill@gmail.com>
2025-10-26 05:43:46 -0400

    update sdk and ndk make immersive view

--- android/app/src/main/java/com/starship/android/MainActivity.java
+++ android/app/src/main/java/com/starship/android/MainActivity.java
@@ -30,7 +30,9 @@ import android.view.View;
 import android.widget.ImageView;
 import android.view.KeyEvent;
 import android.content.pm.ActivityInfo;
-import android.view.KeyEvent;
+import android.os.Build;
+import android.view.WindowInsets;
+import android.view.WindowInsetsController;
 
 public class MainActivity extends SDLActivity {
 static {
@@ -182,6 +184,9 @@ protected void onCreate(Bundle savedInstanceState) {
     userFolderUri = getUserFolderUri();
 
     super.onCreate(savedInstanceState);
+    
+    // Enable immersive mode (edge-to-edge)
+    enableImmersiveMode();
     
 setupControllerOverlay();
     attachController();
@@ -807,6 +812,35 @@ private void setupControllerOverlay() {
     setupJoystick(leftJoystick, leftJoystickKnob, true);
     setupLookAround(rightScreenArea);
     setupToggleButton(buttonToggle, buttonGroup);
+}
+
+private void enableImmersiveMode() {
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+        // Android 11+ (API 30+)
+        WindowInsetsController controller = getWindow().getInsetsController();
+        if (controller != null) {
+            controller.hide(WindowInsets.Type.statusBars() | WindowInsets.Type.navigationBars());
+            controller.setSystemBarsBehavior(WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE);
+        }
+    } else {
+        // Android 10 and below
+        getWindow().getDecorView().setSystemUiVisibility(
+            View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+            | View.SYSTEM_UI_FLAG_FULLSCREEN
+            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+            | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+        );
+    }
+}
+
+@Override
+public void onWindowFocusChanged(boolean hasFocus) {
+    super.onWindowFocusChanged(hasFocus);
+    if (hasFocus) {
+        enableImmersiveMode();
+    }
 }
 
 private void setupMenuButton(Button button) {
